2bc47e4dd998442e2bd2878176973310fea0be6d
kumarnutan204 <kumarnutan204@gmail.com>
2024-02-12 11:21:06 +0530

    [Nutan] Added: Inheritance concept learnt and practiced

--- Inheritance.py
+++ Inheritance.py
@@ -6,7 +6,7 @@ class Employee:
         self.last=last
         self.pay=pay
         self.email= first + '.' + last +'@company.com'
-        Employee.num_of_emp+=1
+        
         
     def fullname(self):
         return f"{self.first} {self.last}"
@@ -14,12 +14,76 @@ class Employee:
         self.pay= int(self.pay*self.raise_amount)
         return self.pay
     
+
+class Developer(Employee):
+    raise_amount=1.10
+    def __init__(self,first,last,pay,prog_lang):
+        #use either of the statemaents to refer to the parent class 
+        #super().__init__(first,last,pay,prog_lang)
+        #Employee.__init__(self,first,last,pay,prog_lang)
+        super().__init__(first,last,pay)
+        self.prog_lang=prog_lang
+            
+class Manager(Employee):
+    def __init__(self,first,last,pay,employees=None):
+        super().__init__(first,last,pay)
+        if employees is None:
+            self.employees=[]
+        else:
+            self.employees=employees
+    def add_emp(self,emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+            
+    def remove_emp(self,emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
     
+    def print_emp(self):
+        for emp in self.employees:
+            print('-->',emp.fullname())
+            
+            
+            
 
-dev1=Employee('Nutan','Kumar', 90000)
-dev2=Employee('A', 'B', 30000)
+dev1=Developer('Nutan','Kumar', 90000,'Python')
+dev2=Developer('A', 'B', 30000,'Java')
 
-print(dev1.email)
-print(dev2.email)
+mgr1=Manager('Sue','Smith',90000,[dev1])
+
+
+
+#Python has functions to check if instance is of a specific class
+
+print(isinstance(mgr1,Manager))#:::: should be true as mgr 1 is an instance of Manager
+
+print(isinstance(mgr1,Developer))
+
+#issubclassfunction
+print(issubclass(Developer,Employee))#should be true as developer is a subclass of Employee
+
+print(issubclass(Manager, Developer))#False
+
+
+
+
+
+
+
+# print(mgr1.email)
+
+# mgr1.add_emp(dev2)
+# mgr1.print_emp()
+# mgr1.remove_emp(dev2)
+# mgr1.print_emp()
+
+
+
+# print(dev1.email)
+# print(dev1.prog_lang)
+
+# print(dev1.pay)
+# dev1.apply_raise()
+# print(dev1.pay)
 
 
