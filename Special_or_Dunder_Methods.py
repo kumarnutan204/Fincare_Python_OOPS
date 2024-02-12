b52fad6d263e73359338d68bb39d037e4b9e6a1a
kumarnutan204 <kumarnutan204@gmail.com>
2024-02-12 12:23:17 +0530

    [Nutan] Edited: Special or Dunder method file

--- Special_or_Dunder_Methods.py
+++ Special_or_Dunder_Methods.py
@@ -1,7 +1,10 @@
 class Employee:
     raise_amount=1.04
-    #anything enclosed with double underscores is called a dunder methodor a special method
-    def __init__(self,first,last,pay):#this init method is a dunder method 
+    #anything enclosed with double underscores is called a 
+    #dunder methodor a special method
+    
+    
+    def __init__(self,first,last,pay):#this init method is a dunder method or a magic method 
         self.first=first
         self.last=last
         self.pay=pay
@@ -13,18 +16,23 @@ class Employee:
     def apply_raise(self):
         self.pay= int(self.pay*self.raise_amount)
         return self.pay
+    
     def __repr__(self): #unambiguous rep of the object and should be used for debugging and logging and things ::mostly use by the developers  
         return f"Employee('{self.first}','{self.last}','{self.pay}')"
+    
+    
     def __str__(self):# more readable representation of an object and is meant to be used as end user display
         return f"{self.fullname()} - {self.email}"
+    
     
     def __add__(self,other):
         return self.pay +other.pay
     
+    
     #get total umber of char in the fullname
     def __len__(self):
         return len(self.fullname())
-    #here we can return "NotImplemented" which ensures that if one dunder method does not did not do the job then it finds other methods of the class to do it 
+    # here we can return "NotImplemented" which ensures that if one dunder method does not did not do the job then it finds other methods of the class to do it 
     
 
 emp1=Employee('Nutan','Kumar', 60000)
